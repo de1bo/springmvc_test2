c20ee48a807c263de4c5d9b1ea520586c4114d89
de1bo <ogsal5765@naver.com>
2022-03-12 01:46:44 +0900

    fg

--- src/main/java/com/junefw/infra/modules/member/Member.java
+++ src/main/java/com/junefw/infra/modules/member/Member.java
@@ -2,22 +2,30 @@ package com.junefw.infra.modules.member;
 
 public class Member {
 
-	private String Seq="";
-	private String ifmmId="";
+	private String seq="";
+	private String ifmmRegistTypeCd = "";
 	private String ifmmName="";
+	private String ifmmId="";
+	private String ifmmPassword= "";
+	private String ifmmBirthday = "";
+	private String ifmmGenderCd;
+	private String ifmmEmailConsentNy = "";
+	private String ifmmPushConsentNy = "";
+	private Integer ifmmDelNy;
+	
 	
 	// ------------------
 	public String getSeq() {
-		return Seq;
+		return seq;
 	}
 	public void setSeq(String seq) {
-		Seq = seq;
+		this.seq = seq;
 	}
-	public String getIfmmId() {
-		return ifmmId;
+	public String getIfmmRegistTypeCd() {
+		return ifmmRegistTypeCd;
 	}
-	public void setIfmmId(String ifmmId) {
-		this.ifmmId = ifmmId;
+	public void setIfmmRegistTypeCd(String ifmmRegistTypeCd) {
+		this.ifmmRegistTypeCd = ifmmRegistTypeCd;
 	}
 	public String getIfmmName() {
 		return ifmmName;
@@ -25,7 +33,53 @@ public class Member {
 	public void setIfmmName(String ifmmName) {
 		this.ifmmName = ifmmName;
 	}
+	public String getIfmmId() {
+		return ifmmId;
+	}
+	public void setIfmmId(String ifmmId) {
+		this.ifmmId = ifmmId;
+	}
+	public String getIfmmPassword() {
+		return ifmmPassword;
+	}
+	public void setIfmmPassword(String ifmmPassword) {
+		this.ifmmPassword = ifmmPassword;
+	}
+	public String getIfmmBirthday() {
+		return ifmmBirthday;
+	}
+	public void setIfmmBirthday(String ifmmBirthday) {
+		this.ifmmBirthday = ifmmBirthday;
+	}
+	public String getIfmmEmailConsentNy() {
+		return ifmmEmailConsentNy;
+	}
+	public void setIfmmEmailConsentNy(String ifmmEmailConsentNy) {
+		this.ifmmEmailConsentNy = ifmmEmailConsentNy;
+	}
+	public String getIfmmPushConsentNy() {
+		return ifmmPushConsentNy;
+	}
+	public void setIfmmPushConsentNy(String ifmmPushConsentNy) {
+		this.ifmmPushConsentNy = ifmmPushConsentNy;
+	}
+	public Integer getIfmmDelNy() {
+		return ifmmDelNy;
+	}
+	public void setIfmmDelNy(Integer ifmmDelNy) {
+		this.ifmmDelNy = ifmmDelNy;
+	}
+	public String getIfmmGenderCd() {
+		return ifmmGenderCd;
+	}
+	public void setIfmmGenderCd(String ifmmGenderCd) {
+		this.ifmmGenderCd = ifmmGenderCd;
+	}
 	
+	
+	
+	
+
 	
 	
 }
